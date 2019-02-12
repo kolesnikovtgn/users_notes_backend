8eed00035e8c5d7b1a4cfcb09917f328d8b94f4f
Kolesnikov Vladimir <lodoss.kolesnikov@gmail.com>
2019-02-12 15:40:29 +0300

    Added auth to all controller in users CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,33 @@
-import { Controller, Get, Post, Body, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Put, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from './interfaces/user.interface';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
+  @UseGuards(AuthGuard('local'))
   async create(@Body() createUserDto: CreateUserDto) {
     this.usersService.create(createUserDto);
   }
 
   @Delete()
+  @UseGuards(AuthGuard('local'))
   async delete(@Body() id) {
     this.usersService.delete(id);
   }
 
   @Put()
+  @UseGuards(AuthGuard('local'))
   async update(@Body() id, @Body() newValue: User) {
     this.usersService.update(id, newValue);
   }
 
   @Get()
+  @UseGuards(AuthGuard('local'))
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
